8cdd5b75b77a6b846566e16f0ae85f830fd83a50
KUMAKO55229 <franckkumako@gmail.com>
2024-06-03 23:09:01 -0300

    Ajustando configuração do server.js

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'web')));
+// Servir os arquivos estáticos da aplicação Flutter
+app.use(express.static(path.join(__dirname, 'build/web')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'web', 'index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/web', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 8080);
