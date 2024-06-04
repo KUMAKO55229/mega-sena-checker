9768d547296b4cf8a4f35a7c152be5f77642df1c
KUMAKO55229 <franckkumako@gmail.com>
2024-06-04 16:27:03 -0300

    Corrigir estrutura de diretórios para deploy no Heroku

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '648eb78fc094cfa2a206d96b60c86d471a29d181',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '648eb78fc094cfa2a206d96b60c86d471a29d181',
+            'dev_requirement' => false,
+        ),
+    ),
+);
