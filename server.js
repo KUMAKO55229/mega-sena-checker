f6a8d27813e46bd6616004dca819b3fc55a4bfb1
KUMAKO55229 <franckkumako@gmail.com>
2024-06-03 23:14:59 -0300

    Ajuste na configuração do server.js e buildpacks

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir os arquivos estáticos da aplicação Flutter
+// Serve os arquivos estáticos da aplicação Flutter
 app.use(express.static(path.join(__dirname, 'build/web')));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build/web', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// Listen to the app on the port provided by Heroku or a default port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
